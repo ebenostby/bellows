981aa8af23cf53db287039ec1c8360ec6da3c721
ebenostby <ebenostby@gmail.com>
2025-07-17 13:29:48 -0400

    Add zigzag to main panel.
    Zigzag code to make slats that extend to the edge. In bellows I've seen the main panels have this and the side panels have straight cuts.

--- bellows.py
+++ bellows.py
@@ -10,12 +10,17 @@
 # pip install svgwrite
 
 import svgwrite
+from svgwrite.extensions import Inkscape
 inch=25.4
 
-def do_bellows(name, page_x, page_y, length, top, bot, other, n_folds, side_space, slack):
-	dwg = svgwrite.Drawing(name, ("%6.1fmm"%page_x, "%6.1fmm"%page_y), profile='tiny')
+def do_bellows(name, page_x, page_y, length, top, bot, other, n_folds, side_space, slack, do_zigzag=1):
+	dwg = svgwrite.Drawing(name, ("%6.1fmm"%page_x, "%6.1fmm"%page_y), profile='full')
+	inkscape = Inkscape(dwg)
 	dwg.viewbox(0, 0, page_x, page_y)	
-
+	cut = inkscape.layer(label="CUT")
+	dwg.add(cut)
+	score = inkscape.layer(label="SCORE")
+	dwg.add(score)
 	slat_y = length / n_folds
 	slat_offset = (other / n_folds)/2
 	slat_1 = (slat_y+slat_offset)/2
@@ -23,21 +28,41 @@ def do_bellows(name, page_x, page_y, length, top, bot, other, n_folds, side_spac
 	top_margin=10
 	
 	# overall shape
-	dwg.add(dwg.polygon( (((page_x-top)/2,top_margin),
-		((page_x+top)/2, top_margin), 
-		((page_x+bot)/2,top_margin+length),
-		((page_x-bot)/2, top_margin+length)), stroke='black',fill='none'))
+	extra_around_cut = do_zigzag*side_space
+	cut.add(dwg.polygon( (((page_x-top)/2-extra_around_cut,top_margin),
+		((page_x+top)/2+extra_around_cut, top_margin), 
+		((page_x+bot)/2+extra_around_cut,top_margin+length),
+		((page_x-bot)/2-extra_around_cut, top_margin+length)), stroke='black',fill='none'))
 	# individual stiffeners or slats
 	for i in range(n_folds):
 		cursor=length*i/n_folds
+		next_cursor=length*(i+1)/n_folds
 
-		for y_pos in ( cursor+slat_1-slack, cursor+slat_1+slat_2-slack):
-			width=top+(y_pos/length)*(bot-top)-2*side_space
-			dwg.add(dwg.rect(((page_x-width)/2,y_pos+top_margin),(width,slack), stroke='black',fill='none'))
+		if (do_zigzag):
+			y_pos = cursor+slat_1-slack
+			y2_pos = cursor+slat_1+slat_2-slack
+
+			width1=top+(cursor/length)*(bot-top)-2*side_space
+			width2=top+(y_pos/length)*(bot-top)
+			width3=top+(y2_pos/length)*(bot-top)-2*side_space
+			cut.add(dwg.rect(((page_x-width2)/2,y_pos+top_margin),(width2,slack), stroke='black',fill='none'))
+			score.add(dwg.line( ((page_x-width1)/2,cursor+top_margin),((page_x-width2)/2,y_pos+top_margin),stroke='green'))
+			score.add(dwg.line( ((page_x+width1)/2,cursor+top_margin),((page_x+width2)/2,y_pos+top_margin),stroke='green'))
+			score.add(dwg.line( ((page_x-width2)/2,y_pos+top_margin+slack), ((page_x-width3)/2,y2_pos+top_margin),stroke='green'))
+			score.add(dwg.line( ((page_x+width2)/2,y_pos+top_margin+slack), ((page_x+width3)/2,y2_pos+top_margin),stroke='green'))
+			y_pos = y2_pos
+			width1=top+(cursor/length)*(bot-top)
+			width2=top+(y_pos/length)*(bot-top)-2*side_space
+			cut.add(dwg.rect(((page_x-width2)/2,y_pos+top_margin),(width2,slack), stroke='black',fill='none'))
+		else:	
+			for y_pos in ( cursor+slat_1-slack, cursor+slat_1+slat_2-slack):
+				width=top+(y_pos/length)*(bot-top)-2*side_space			
+				cut.add(dwg.rect(((page_x-width)/2,y_pos+top_margin),(width,slack), stroke='black',fill='none'))
 	
 	# score line
-	dwg.add(dwg.line( ((page_x-top)/2+side_space,top_margin), ((page_x-bot)/2+side_space, top_margin+length), stroke='green'))
-	dwg.add(dwg.line( ((page_x+top)/2-side_space,top_margin), ((page_x+bot)/2-side_space, top_margin+length), stroke='green'))
+	if (not do_zigzag):
+		score.add(dwg.line( ((page_x-top)/2+side_space,top_margin), ((page_x-bot)/2+side_space, top_margin+length), stroke='green'))
+		score.add(dwg.line( ((page_x+top)/2-side_space,top_margin), ((page_x+bot)/2-side_space, top_margin+length), stroke='green'))
 
 	dwg.save()
 	
@@ -61,11 +86,11 @@ def main():
 	n_folds = 18
 	# room between edge of the slat and the corner of the bellows fabric on each side. 
 	# The zig-zag occupies this space.
-	side_space = 10
+	side_space = 5
 	# the space between slats where folds are located.
 	slat_slack = 1.5
 
-	do_bellows(b1_name,page_x, page_y, length, b1_top, b1_bot, b2_top-b2_bot, n_folds, side_space, slat_slack)
-	do_bellows(b2_name,page_x, page_y, length, b2_bot, b2_top, b1_top-b1_bot, n_folds, side_space, slat_slack)
+	do_bellows(b1_name,page_x, page_y, length, b1_top, b1_bot, b2_top-b2_bot, n_folds, side_space, slat_slack, do_zigzag=1)
+	do_bellows(b2_name,page_x, page_y, length, b2_bot, b2_top, b1_top-b1_bot, n_folds, side_space, slat_slack, do_zigzag=0)
 
 main()
